9da6b26920a94232e3c7a71b08fe751ea8bcafdb
Gabriela Cardozo <gabriela@git.faccat.br>
2018-06-12 11:35:06 -0300

    Agora, o robô deve andar alguns segundos para frente, parar por alguns segundos, e novamente andar mais alguns segundos, parar novamente, andar novamente. Repetir essa sequência de andar – parar – andar – parar – andar.

---
+++ Trabalho_Arduino/t4/t4.ino
@@ -0,0 +1,30 @@
+#include<Servo.h>
+
+int ver = 4 ;
+int amar = 3;
+int verde = 2;
+int chavedir = 6;
+int nivchavedir;
+int motesq = 9;
+int motdir = 8;
+
+Servo servoEsq; //classe e obj
+Servo servoDir; // classe e obj
+
+void setup() {
+// put your setup code here, to run once:
+ servoDir.attach(motdir);
+
+  servoEsq.attach(motesq);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  servoDir.writeMicroseconds(2100);
+  servoEsq.writeMicroseconds(900);
+  delay(2000);
+  servoDir.writeMicroseconds(1500);
+  servoEsq.writeMicroseconds(1500);
+  delay(2000);
+  
+}
