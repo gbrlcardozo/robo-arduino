5caa60169e3e91d1fd67345b293b490b6d5bab43
Gabriela Cardozo <gabriela@git.faccat.br>
2018-06-12 11:34:16 -0300

    piscar leds em sequencia

---
+++ Trabalho_Arduino/t2/t2.ino
@@ -0,0 +1,26 @@
+int ver = 4 ;
+int amar = 3;
+int verde = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ver, OUTPUT);
+pinMode(amar, OUTPUT);
+pinMode(verde, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(verde, HIGH);
+ delay(1000);
+ digitalWrite(verde, LOW);
+ delay(1000); 
+ digitalWrite(amar, HIGH);
+ delay(1000);
+ digitalWrite(amar, LOW);
+ delay(1000); 
+ digitalWrite(ver, HIGH);
+ delay(1000);
+ digitalWrite(ver, LOW);
+ delay(1000); 
+}
