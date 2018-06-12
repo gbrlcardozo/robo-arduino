6870b8fe42405bc0b0a9f8616a727dc4893eafa3
Gabriela Cardozo <gabriela@git.faccat.br>
2018-06-12 11:33:54 -0300

    piscar led

---
+++ Trabalho_Arduino/t1/t1.ino
@@ -0,0 +1,13 @@
+int ver = 4;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ver, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(ver, HIGH);
+ delay(1000);
+ digitalWrite(ver, LOW);
+ delay(1000); 
+}
