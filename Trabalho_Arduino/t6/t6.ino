ef640dea99d8e7feb34f3d5fb1420198b71c03be
Gabriela Cardozo <gabriela@git.faccat.br>
2018-06-12 11:35:50 -0300

    Agora o robô deve andar para frente e quando tocar a chave esquerda, parar por 1 segundo e voltar para trás por alguns segundos. Aí, parar novamente, e avançar para frente novamente... se tocar na chave esquerda novamente, parar novamente 1 segundo e voltar para trás por alguns segundos. Aí, parar novamente, e avançar... (ciclo – loop – infinito), sempre repetindo a mesma função infinitamente.

---
+++ Trabalho_Arduino/t6/t6.ino
@@ -0,0 +1,39 @@
+#include<Servo.h>
+
+int ver = 4 ;
+int amar = 3;
+int verde = 2;
+int chavedir = 6;
+int nivchavedir;
+int motesq = 9;
+int motdir = 8;
+int chaveesq = 7;
+int nivchaveesq;
+
+Servo servoEsq; //classe e obj
+Servo servoDir; // classe e obj
+
+void setup() {
+// put your setup code here, to run once:
+ servoDir.attach(motdir);
+
+ servoEsq.attach(motesq);
+
+ pinMode(chaveesq, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  nivchaveesq = digitalRead(chaveesq);
+  if(chaveesq == HIGH){
+      servoDir.writeMicroseconds(2100);
+      servoEsq.writeMicroseconds(900);
+  }else{
+  servoDir.writeMicroseconds(1500);
+  servoEsq.writeMicroseconds(1500);
+  delay(2000);
+  servoDir.writeMicroseconds(900);
+  servoEsq.writeMicroseconds(2100);
+  delay(2000);
+  }
+}
